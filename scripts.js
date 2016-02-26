b359348b233b54576b8a09440fb2587db5815139
ErikBoesen <me@erikboesen.com>
2016-02-26 16:09:37 -0500

    Cancel button and user display thing

--- scripts.js
+++ scripts.js
@@ -3,11 +3,19 @@ var controls = {
 	path: document.getElementById('path'),
 	message: document.getElementById('message'),
 	commit: document.getElementById('commit'),
-	pull: document.getElementById('pull')
+	pull: document.getElementById('pull'),
+	cancel: document.getElementById('cancel')
 };
 controls.commit.onclick = function() {
+    controls.commit.innerHTML = 'Push';
 	controls.commit.classList.add('active');
-    controls.pull.classList.add('active');
-    controls.pull.innerHTML = 'Cancel';
+	controls.pull.classList.add('active');
+	controls.cancel.classList.add('active');
 	controls.message.focus();
+};
+controls.cancel.onclick = function() {
+    controls.commit.innerHTML = 'Commit';
+	controls.commit.classList.remove('active');
+	controls.pull.classList.remove('active');
+	controls.cancel.classList.remove('active');
 };
