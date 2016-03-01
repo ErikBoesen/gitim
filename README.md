ea5df1d3aef362b450e7d2fbadeaa03460f5b1b9
ErikBoesen <me@erikboesen.com>
2016-03-01 09:32:57 -0500

    Nonfunctional sign-in page

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 Extremely light and minimal Git client.
 
 ## Installation
-_Coming soon!_
+¯\\\_(ツ)\_/¯
 
-## Author
-[Erik Boesen](https://github.com/ErikBoesen)
-Logo designed by [James Fenn](https://github.com/TheAndroidMaster).
+## Authors
+* [Erik Boesen](https://github.com/ErikBoesen)
+* Logo by [James Fenn](https://github.com/TheAndroidMaster).
 
 ## License
 This code is public domain under the CC0 license. Do whatever you want with it. I'd prefer if you give me credit, but you don't necessarily _have_ to.
--- scripts.js
+++ scripts.js
@@ -1,21 +1,33 @@
 var controls = {
-	url: document.getElementById('url'),
-	path: document.getElementById('path'),
-	message: document.getElementById('message'),
-	commit: document.getElementById('commit'),
-	pull: document.getElementById('pull'),
-	cancel: document.getElementById('cancel')
+    signIn: {
+        this: document.getElementById('sign-in-section'),
+        username: document.getElementById('username'),
+        password: document.getElementById('password'),
+        login: document.getElementById('login')
+    },
+    main: {
+    	url: document.getElementById('url'),
+    	path: document.getElementById('path'),
+    	message: document.getElementById('message'),
+    	commit: document.getElementById('commit'),
+    	pull: document.getElementById('pull'),
+    	cancel: document.getElementById('cancel')
+    }
 };
-controls.commit.onclick = function() {
-    controls.commit.innerHTML = 'Push';
-	controls.commit.classList.add('active');
-	controls.pull.classList.add('active');
-	controls.cancel.classList.add('active');
-	controls.message.focus();
+controls.main.commit.onclick = function() {
+    controls.main.commit.innerHTML = 'Push';
+	controls.main.commit.classList.add('active');
+	controls.main.pull.classList.add('active');
+	controls.main.cancel.classList.add('active');
+	controls.main.message.focus();
 };
-controls.cancel.onclick = function() {
-    controls.commit.innerHTML = 'Commit';
-	controls.commit.classList.remove('active');
-	controls.pull.classList.remove('active');
-	controls.cancel.classList.remove('active');
+controls.main.cancel.onclick = function() {
+    controls.main.commit.innerHTML = 'Commit';
+	controls.main.commit.classList.remove('active');
+	controls.main.pull.classList.remove('active');
+	controls.main.cancel.classList.remove('active');
+};
+controls.signIn.login.onclick = function() {
+    console.log('test');
+    controls.signIn.this.style.display = 'none';
 };
